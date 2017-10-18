e07ade3a621a1cc72426f06ad5b7139dccd65ea2
George Gaylor <georgegaylor83@outlook.com>
2017-10-17 22:12:40 -0700

    Created Home Controller/View added to Startup

---
+++ Lab26George/Lab26George/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab26George.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
